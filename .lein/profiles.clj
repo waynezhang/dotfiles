ff175f920dcc633f57b46b4295f1d5627a2c3f45
Linghua Zhang <linghua.zhang@me.com>
2013-08-06 12:13:55 +0900

    add. leinigen

---
+++ .lein/profiles.clj
@@ -0,0 +1,5 @@
+{:user 
+ {:android  {:sdk-path "/Users/wayne/adt-bundle-mac/sdk/"} 
+  :plugins [[lein-droid "0.2.0-SNAPSHOT"]
+            [lein-ancient "0.4.4"]
+            [lein-localrepo "0.5.2"]]}}
