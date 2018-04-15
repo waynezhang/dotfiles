4359ceea82f1916f5cefafa08f28fbcb9dc267e8
Linghua Zhang <r_cho@r.recruit.co.jp>
2018-04-15 12:13:06 +0900

    [hammerspoon] maintaince

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -63,7 +63,7 @@ hs.hotkey.bind({ "shift", "alt" }, "up", move(0, -0.1))
 hs.hotkey.bind({ "shift", "alt" }, "down", move(0, 0.1))
 
 -- window counterpart
-hs.hotkey.bind({"alt"}, "Tab", function()
+hs.hotkey.bind({"alt"}, "tab", function()
   local app = hs.application.frontmostApplication()
   local windows = app:allWindows()
 
@@ -91,7 +91,7 @@ end)
 
 -- Input Method Switch
 hs.hotkey.bind({"cmd"}, "kana", function()
-  hs.keycodes.currentSourceID("com.apple.inputmethod.SCIM.ITABC")
+  hs.keycodes.currentSourceID("com.googlecode.rimeime.inputmethod.Squirrel.Rime")
 end)
 
 -- Spotify
@@ -103,26 +103,18 @@ function showCurrentSpotifyTrack()
 end
 hs.hotkey.bind({"cmd", "alt"}, "right", function()
   hs.spotify.next()
-  showCurrentSpotifyTrack()
+  hs.timer.doAfter(0.1, function()
+    showCurrentSpotifyTrack()
+  end)
 end)
 hs.hotkey.bind({"cmd", "alt"}, "left", function()
   hs.spotify.previous()
-  showCurrentSpotifyTrack()
+  hs.timer.doAfter(0.1, function()
+    showCurrentSpotifyTrack()
+  end)
 end)
 
 hs.hotkey.bind({"cmd", "alt"}, "space", function()
   hs.spotify.playpause()
 end)
 
-
--- Wi-Fi SSID
-local wifiMenu = hs.menubar.new()
-wifiMenu:setTitle(hs.wifi.currentNetwork())
-hs.wifi.watcher.new(function()
-  local wifiName = hs.wifi.currentNetwork()
-  if wifiName then
-    wifiMenu:setTitle(wifiName)
-  else
-    wifiMenu:setTitle("Wifi OFF")
-  end
-end):start()
