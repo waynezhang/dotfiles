877a731efad5a90a91a5ebce6c9f7cb082fb9e73
Linghua Zhang <r_cho@r.recruit.co.jp>
2018-05-23 15:30:24 +0900

    [osx] add. homebrew, zplugin, etc...

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -1,7 +1,20 @@
-#!/bin/zsh
+#!/usr/bin/env bash
 
+# Ask for the administrator password upfront
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until `.macos` has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+# Install Homebrew
 if test ! $(which brew); then
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
 brew bundle
+
+# Install zplugin
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zplugin/master/doc/install.sh)"
+sudo dscl . -change /Users/$USER UserShell /bin/bash /usr/local/bin/zsh
+
+sh osx.sh
