b84b33cdf5b3a3f8a47120a15301a8b5f9e0dc75
Linghua Zhang <linghua.zhang@me.com>
2021-12-29 01:17:22 +0900

    fix vim colors

--- .config/nvim/lua/colors.lua
+++ .config/nvim/lua/colors.lua
@@ -1,18 +1,12 @@
 require('paq-nvim').paq { 'nanotech/jellybeans.vim', as = 'jellybeans' }
 
-require('common').set_options({
-  -- color cursorline = true,
-})
-
-vim.api.nvim_command([[
-  augroup ChangeBackgroudColour
-    autocmd colorscheme * :highlight normal ctermbg=none
-    autocmd colorscheme * :highlight NonText ctermbg=none
-    autocmd colorscheme * :highlight CursorLine cterm=none ctermbg=236
-    autocmd colorscheme * :highlight SignColumn ctermbg=black
-    autocmd colorscheme * :highlight SignifySignAdd ctermbg=black ctermfg=green
-    autocmd colorscheme * :highlight SignifySignDelete ctermbg=black ctermfg=red
-    autocmd colorscheme * :highlight SignifySignChange ctermbg=black ctermfg=yellow
-  augroup END
-]])
+vim.g.jellybeans_overrides = {
+	background = { guibg = 'none' },
+	Visual = { guibg = '5050505' },
+	LineNr = { guifg = '696969' },
+	NonText = { guifg = '696969' },
+	SignifySignAdd = { guifg = '00ff00' },
+	SignifySignDelete = { guifg = 'ff0000' },
+	SignifySignChange = { guifg = 'ffff00' },
+}
 vim.cmd('silent! colorscheme jellybeans')
